ba58a8ae6901a406ff5cba557ba1d4a71fc38245
David Barbosa <david.barbosa@adene.pt>
2025-08-07 16:09:50 +0100

    GetProductById and GetProcuts endpoints.

---
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductById
+{
+
+    public record GetProductBydIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+
+    internal class GetProductByIdHandler
+        (IDocumentSession session, ILogger logger)
+        : IQueryHandler<GetProductBydIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductBydIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdHandler. Handle called with {@query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if (product is null)
+                throw new ProductNotFoundException();
+
+            return new GetProductByIdResult(product);
+        }
+    }
+}
